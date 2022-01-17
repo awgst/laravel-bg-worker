c5bd512c4834f0eb289512282a11f782a45d5c65
awgst <trisaktiawang@gmail.com>
2022-01-17 07:21:00 +0700

    worker

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    TestJob::dispatch('Hello Job');
     return view('welcome');
 });
